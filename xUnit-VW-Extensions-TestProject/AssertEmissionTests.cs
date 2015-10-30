ca772812141b9f0a94545901a79ca7fd45674cb7
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-10-30 22:53:44 +0100

    Add source code

---
+++ xUnit-VW-Extensions-TestProject/AssertEmissionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace xUnit_VW_Extensions_TestProject
+{
+    using Xunit;
+    using Assert = Xunit.VW.Assert;
+
+    public class AssertEmissionTests
+	{
+		[Fact]
+		public void EmissionTests()
+		{
+			const int Emissions = 9001;
+			const int LegalLimit = 1337;
+			
+			Assert.InRange(Emissions, 0, LegalLimit);			
+		}
+	}
+}
---
+++ xUnit-VW-Extensions-TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xUnit-VW-Extensions-TestProject")]
+[assembly: AssemblyProduct("xUnit-VW-Extensions-TestProject")]
+[assembly: AssemblyCopyright("Copyright © Eirik M Hammerstad 2015")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C5001903-B6E4-4632-A34C-3CE251BB0022")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
